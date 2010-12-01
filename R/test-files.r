a763d0deaceea7a4eba67e1ba20c4d65e5315df7
homer <homer.strong@gmail.com>
2010-12-01 09:50:58 -0800

    added env argument, testing for either file or folder, and a first pass at test_each

--- R/test-files.r
+++ R/test-files.r
@@ -49,8 +49,62 @@ source_dir <- function(path, pattern = "\\.[rR]$", env = NULL, chdir = TRUE) {
 #' @param reporter reporter to use
 #' @param chdir whether to change directories for sourcing
 #' @export
-test_file <- function(path, reporter = "summary", chdir = TRUE) {    
+test_file <- function(path, reporter = "summary", env = NULL, chdir = TRUE) {    
   reporter <- find_reporter(reporter)
+  if (is.null(env)) {
+    env <- new.env(parent = globalenv())
+  }
+
   with_reporter(reporter, 
-    sys.source(path, new.env(parent = globalenv()), chdir))
+    sys.source(path, env, chdir))
+}
+
+
+#' Run all tests on whatever.
+#'
+#' Test using either test_file or test_dir, depending on whether path is a directory
+#'
+#' @param path path to file
+#' @param reporter reporter to use
+#' @param chdir whether to change directories for sourcing
+#' @export
+test_whatever <- function(path, reporter = "summary", env = NULL, chdir = TRUE) {
+  (if (file.info(path)$isdir) {
+    test_dir
+  } else {
+    test_file
+  })(path, reporter, env, chdir)
+}
+
+#' Run a test suite on each object in a named list.
+#'
+#' To be used when a list of objects should be
+#' tested with the same suite of tests. 
+#'
+#' So the suite should be testing, in the default case, the 
+#' object \code{Obj}. In its verbosity, the test suite may
+#' use \code{context(Name)} or something similar.
+#'
+#' @param things a list of objects to test
+#' @param suite path to a file or folder of tests
+#' @param aliases generic names for the object and the object's name
+#' @param parentenv the parent environment for the test environments
+#' @param ... to test_whatever
+#' @export
+test_each <- function(things, suite,
+                      aliases = list(name="Name", obj="Obj"),
+                      parentenv = globalenv(), ...) {
+
+  if (is.null(names(things)))
+    names(things) <- length(things)
+  
+  mapply(function(obj, name) {
+    env <- new.env(parent = parentenv)
+    assign(aliases$name, name, envir = env)
+    assign(aliases$obj, obj, envir = env)
+    test_whatever(suite, env = env, ...)
+  },
+         things,
+         names(things),
+         SIMPLIFY = FALSE) 
 }
